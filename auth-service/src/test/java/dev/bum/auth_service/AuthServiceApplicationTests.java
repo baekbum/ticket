825ffa92d388c7bf951ca53bf1abe310d0607789
baekbum <baekbum828@naver.com>
2026-03-31 22:19:14 +0900

    Init auth-service

---
+++ auth-service/src/test/java/dev/bum/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bum.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
