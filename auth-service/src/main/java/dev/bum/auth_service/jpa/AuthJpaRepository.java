5aad17028c06d66deb6e0cec2ea51e257ea7fcec
baekbum <baekbum828@naver.com>
2026-04-07 22:55:13 +0900

    Revert "[Auth] JwtTokenProvider 공통 모듈로 이동"

--- auth-service/src/main/java/dev/bum/auth_service/jpa/AuthJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.bum.auth_service.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface AuthJpaRepository extends JpaRepository<Auth, Long> {
-    Optional<Auth> findByUserId(String userId);
-}
--- auth-service/src/main/java/dev/bum/auth_service/jpa/AuthRepository.java
+++
@@ -1,6 +0,0 @@
-package dev.bum.auth_service.jpa;
-
-public interface AuthRepository {
-    Auth findById(Long id);
-    Auth findByUserId(String userId);
-}
