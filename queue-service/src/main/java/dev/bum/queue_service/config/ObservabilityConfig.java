8546236ea6bcce41642f8e3225a020427bf672e6
baekbum <baekbum828@naver.com>
2026-08-09 21:17:38 +0900

    분산 추적 환경 구성

---
+++ queue-service/src/main/java/dev/bum/queue_service/config/ObservabilityConfig.java
@@ -0,0 +1,20 @@
+package dev.bum.queue_service.config;
+
+import io.micrometer.observation.ObservationPredicate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.observation.ServerRequestObservationContext;
+
+@Configuration
+public class ObservabilityConfig {
+
+    @Bean
+    public ObservationPredicate actuatorServerRequestObservationPredicate() {
+        return (name, context) -> {
+            if (context instanceof ServerRequestObservationContext serverContext) {
+                return !serverContext.getCarrier().getRequestURI().contains("/actuator");
+            }
+            return true;
+        };
+    }
+}
